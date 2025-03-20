524dd8e64a8701cacdcacd35cc9c17352306bb1e
Philipp Moritz <pcmoritz@gmail.com>
2025-03-20 04:52:34 +0000

    update

--- aten/src/ATen/native/tt/Kernels.cpp
+++ aten/src/ATen/native/tt/Kernels.cpp
@@ -1,8 +1,30 @@
 #include <ATen/ops/add_native.h>
 
+#include <tt-metalium/host_api.hpp>
+
+using namespace tt::tt_metal;
+
 namespace at::native {
 
 at::Tensor & add_out_tt(const at::Tensor & self, const at::Tensor & other, const at::Scalar & alpha, at::Tensor & out) {
+  // TODO: Implement
+  return out;
+}
+
+at::Tensor & add_out_tt(const at::Tensor & self, const at::Tensor & other, const at::Scalar & alpha, at::Tensor & out) {
+  Program program = CreateProgram();
+  constexpr CoreCoord core = {0, 0};
+  KernelHandle binary_reader_kernel_id = CreateKernel(
+    program,
+    "tt_metal/kernels/dataflow/reader_binary_diff_lengths.cpp",
+    core,
+    DataMovementConfig{.processor = DataMovementProcessor::RISCV_1, .noc = NOC::RISCV_1_default});
+  KernelHandle unary_writer_kernel_id = CreateKernel(
+    program,
+    "tt_metal/kernels/dataflow/writer_unary.cpp",
+    core,
+    DataMovementConfig{.processor = DataMovementProcessor::RISCV_0, .noc = NOC::RISCV_0_default});
+  
   return out;
 }
 
