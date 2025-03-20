39d85071456b4a6dfd1511b6230ee71b25edda0f
Philipp Moritz <pcmoritz@gmail.com>
2025-03-20 00:02:45 -0700

    update

--- aten/src/ATen/tt/TTDevice.cpp
+++ aten/src/ATen/tt/TTDevice.cpp
@@ -12,58 +12,29 @@ using namespace tt::tt_metal;
 
 namespace at::tt {
 
-class TTAllocator : public c10::Allocator {
-public:
-  TTAllocator() {
-    device_ = CreateDevice(0);
-  }
-  virtual void emptyCache() const {}
-  virtual void freeInactiveBuffers() const {}
-  virtual ssize_t getUnalignedBufferSize(const void* ptr) const { return -1; }
-  virtual IntArrayRef getBufferShape(const void* ptr) const { return IntArrayRef(); }
-  virtual id_t getBufferId(const void* ptr) const { return 0; }
-  virtual void setBufferShape(const void* ptr, const IntArrayRef& shape)
-    const {}
-  virtual bool isSharedBuffer(const void* ptr) const { return false; }
-  virtual bool isSharedStorageSupported() const { return false; }
-  virtual c10::DataPtr allocScalarBufferWithValue(void* value, size_t size)
-    const { return DataPtr(); }
-  virtual std::string formatSize(size_t size) const { return ""; }
-  virtual void setLowWatermarkRatio(double ratio) const {}
-  virtual void setHighWatermarkRatio(double ratio) const {}
-  virtual ssize_t getLowWatermarkValue() const { return -1; }
-  virtual size_t getLowWatermarkLimit() const { return 0; }
-  virtual size_t getHighWatermarkLimit() const { return 0; }
-  virtual size_t getTotalAllocatedMemory() const { return 0; }
-  virtual size_t getCurrentAllocatedMemory() const { return 0; }
-  virtual size_t getDriverAllocatedMemory() const { return 0; }
-  virtual size_t getRecommendedMaxMemory() const { return 0; }
-  virtual std::pair<const void*, uint32_t> getSharedBufferPtr(const void* ptr) const { return {nullptr, 0}; }
-  virtual bool recordEvents(c10::ArrayRef<const void*> buffers) const { return false; }
-  virtual bool waitForEvents(c10::ArrayRef<const void*> buffers) const {return false; }
+TTAllocator::TTAllocator() {
+  device_ = CreateDevice(0);
+}
 
-  virtual DataPtr allocate(size_t n) {
-    std::cout << "allocating " << n << std::endl;
-    InterleavedBufferConfig config{
-        .device = device_,
-        .size = n,
-        .page_size = n,
-        .buffer_type = BufferType::DRAM
-    };
-    buffers_.push_back(CreateBuffer(config));
-    return DataPtr(reinterpret_cast<void*>(buffers_.back()->address()), DeviceType::TT);
-  }
-  virtual void copy_data(void* dest, const void* src, std::size_t count) const {
-    CommandQueue& cq = device_->command_queue();
-    EnqueueWriteBuffer(cq, buffers_[0], src, false); // TODO(pcm): Fix this
-    Finish(cq);
-  }
-private:
-  std::vector<std::shared_ptr<Buffer>> buffers_;
-  IDevice* device_;
-};
+virtual DataPtr TTAllocator::allocate(size_t n) {
+  std::cout << "allocating " << n << std::endl;
+  InterleavedBufferConfig config{
+      .device = device_,
+      .size = n,
+      .page_size = n,
+      .buffer_type = BufferType::DRAM
+  };
+  buffers_.push_back(CreateBuffer(config));
+  return DataPtr(reinterpret_cast<void*>(buffers_.back()->address()), DeviceType::TT);
+}
 
-at::Allocator* GetTTAllocator(bool useSharedAllocator) {
+virtual void TTAllocator::copy_data(void* dest, const void* src, std::size_t count) const {
+  CommandQueue& cq = device_->command_queue();
+  EnqueueWriteBuffer(cq, buffers_[0], src, false); // TODO(pcm): Fix this
+  Finish(cq);
+}
+
+TTAllocator* GetTTAllocator(bool useSharedAllocator) {
   static TTAllocator allocator;
   return &allocator;
 }
--- aten/src/ATen/tt/TTDevice.h
+++ aten/src/ATen/tt/TTDevice.h
@@ -5,6 +5,43 @@
 
 namespace at::tt {
 
-TORCH_API at::Allocator* GetTTAllocator(bool useSharedAllocator = false);
+class TTAllocator : public c10::Allocator {
+public:
+  TTAllocator();
+
+  virtual void emptyCache() const {}
+  virtual void freeInactiveBuffers() const {}
+  virtual ssize_t getUnalignedBufferSize(const void* ptr) const { return -1; }
+  virtual IntArrayRef getBufferShape(const void* ptr) const { return IntArrayRef(); }
+  virtual id_t getBufferId(const void* ptr) const { return 0; }
+  virtual void setBufferShape(const void* ptr, const IntArrayRef& shape)
+    const {}
+  virtual bool isSharedBuffer(const void* ptr) const { return false; }
+  virtual bool isSharedStorageSupported() const { return false; }
+  virtual c10::DataPtr allocScalarBufferWithValue(void* value, size_t size)
+    const { return DataPtr(); }
+  virtual std::string formatSize(size_t size) const { return ""; }
+  virtual void setLowWatermarkRatio(double ratio) const {}
+  virtual void setHighWatermarkRatio(double ratio) const {}
+  virtual ssize_t getLowWatermarkValue() const { return -1; }
+  virtual size_t getLowWatermarkLimit() const { return 0; }
+  virtual size_t getHighWatermarkLimit() const { return 0; }
+  virtual size_t getTotalAllocatedMemory() const { return 0; }
+  virtual size_t getCurrentAllocatedMemory() const { return 0; }
+  virtual size_t getDriverAllocatedMemory() const { return 0; }
+  virtual size_t getRecommendedMaxMemory() const { return 0; }
+  virtual std::pair<const void*, uint32_t> getSharedBufferPtr(const void* ptr) const { return {nullptr, 0}; }
+  virtual bool recordEvents(c10::ArrayRef<const void*> buffers) const { return false; }
+  virtual bool waitForEvents(c10::ArrayRef<const void*> buffers) const {return false; }
+
+  virtual DataPtr TTAllocator::allocate(size_t n);
+  virtual void copy_data(void* dest, const void* src, std::size_t count) const;
+
+private:
+  std::vector<std::shared_ptr<Buffer>> buffers_;
+  IDevice* device_;
+};
+
+TORCH_API TTAllocator* GetTTAllocator(bool useSharedAllocator = false);
 
 } // namespace at::tt
