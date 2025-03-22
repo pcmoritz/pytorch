91b4720534ff9a62e548cb06bf8b15f42abe59ae
Philipp Moritz <pcmoritz@gmail.com>
2025-03-22 19:27:12 +0000

    fix compilation

--- aten/src/ATen/native/tt/Kernels.cpp
+++ aten/src/ATen/native/tt/Kernels.cpp
@@ -3,14 +3,24 @@
 #include <ATen/tt/TTDevice.h>
 
 #include <tt-metalium/host_api.hpp>
+#include <tt-metalium/work_split.hpp>
 
+using namespace tt;
 using namespace tt::tt_metal;
+
+using CoreSpec = std::variant<CoreCoord, CoreRange, CoreRangeSet>;
 
 namespace at::native {
 
-CBHandle MakeCircularBufferF32(Program& program, const CoreSpec& core, tt::CBIndex cb, uint32_t n_tiles) {
-  constexpr uint32_t tile_size = sizeof(float) * tt::constants::TILE_WIDTH * tt::constants::TILE_HEIGHT;
-  return MakeCircularBuffer(program, core, cb, n_tiles * tile_size, tile_size, tt::DataFormat::Float32);
+static CBHandle MakeCircularBuffer(
+    Program& program, const CoreSpec& core, CBIndex cb, uint32_t size, uint32_t page_size, DataFormat format) {
+    CircularBufferConfig cb_src0_config = CircularBufferConfig(size, {{cb, format}}).set_page_size(cb, page_size);
+    return CreateCircularBuffer(program, core, cb_src0_config);
+}
+
+static CBHandle MakeCircularBufferF32(Program& program, const CoreSpec& core, CBIndex cb, uint32_t n_tiles) {
+  constexpr uint32_t tile_size = sizeof(float) * constants::TILE_HW;
+  return MakeCircularBuffer(program, core, cb, n_tiles * tile_size, tile_size, DataFormat::Float32);
 }
 
 at::Tensor & add_out_tt(const at::Tensor & self, const at::Tensor & other, const at::Scalar & alpha, at::Tensor & out) {
@@ -18,6 +28,8 @@ at::Tensor & add_out_tt(const at::Tensor & self, const at::Tensor & other, const
   auto* device = allocator->device();
   CommandQueue& cq = device->command_queue();
   Program program = CreateProgram();
+
+  const uint32_t n_tiles = (self.numel() + ::tt::constants::TILE_HW - 1) / ::tt::constants::TILE_HW;
 
   auto a = allocator->get_buffer(self.data_ptr());
   auto b = allocator->get_buffer(other.data_ptr());
@@ -30,13 +42,13 @@ at::Tensor & add_out_tt(const at::Tensor & self, const at::Tensor & other, const
   auto all_device_cores = CoreRange({0, 0}, {num_cores_x - 1, num_cores_y - 1});
 
   const uint32_t cir_buf_num_title = 4;
-  CBHandle cb_a = MakeCircularBufferF32(program, all_device_cores, tt::CBIndex::c_0, cir_buf_num_title);
-  CBHandle cb_b = MakeCircularBufferF32(program, all_device_cores, tt::CBIndex::c_1, cir_buf_num_title);
-  CBHandle cb_c = MakeCircularBufferF32(program, all_device_cores, tt::CBIndex::c_2, cir_buf_num_title);
+  CBHandle cb_a = MakeCircularBufferF32(program, all_device_cores, CBIndex::c_0, cir_buf_num_title);
+  CBHandle cb_b = MakeCircularBufferF32(program, all_device_cores, CBIndex::c_1, cir_buf_num_title);
+  CBHandle cb_c = MakeCircularBufferF32(program, all_device_cores, CBIndex::c_2, cir_buf_num_title);
 
-  std::vector<uint32_t> reader_compile_time_args = {(uint32_t)tt::CBIndex::c_0, (uint32_t)tt::CBIndex::c_1};
-  std::vector<uint32_t> writer_compile_time_args = {(uint32_t)tt::CBIndex::c_2};
-  std::vector<uint32_t> compute_compile_time_args = {(uint32_t)tt::CBIndex::c_0, (uint32_t)tt::CBIndex::c_1, (uint32_t)tt::CBIndex::c_2};
+  std::vector<uint32_t> reader_compile_time_args = {(uint32_t)CBIndex::c_0, (uint32_t)CBIndex::c_1};
+  std::vector<uint32_t> writer_compile_time_args = {(uint32_t)CBIndex::c_2};
+  std::vector<uint32_t> compute_compile_time_args = {(uint32_t)CBIndex::c_0, (uint32_t)CBIndex::c_1, (uint32_t)CBIndex::c_2};
 
   auto reader = CreateKernel(
       program,
@@ -65,7 +77,7 @@ at::Tensor & add_out_tt(const at::Tensor & self, const at::Tensor & other, const
 
   constexpr bool row_major = true;
   auto [num_cores, all_cores, core_group_1, core_group_2, num_tiles_per_core_group_1, num_tiles_per_core_group_2] =
-      tt::tt_metal::split_work_to_cores(compute_with_storage_grid_size, n_tiles, row_major);
+      split_work_to_cores(grid_size, n_tiles, row_major);
 
   auto cores = grid_to_cores(num_cores_total, num_cores_x, num_cores_y, row_major);
   for (uint32_t i = 0, start_tile_id = 0; i < num_cores_total; i++) {
