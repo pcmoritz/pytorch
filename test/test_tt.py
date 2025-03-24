1375e058319098c24fa2b01a1d6d3a37949d16c1
Philipp Moritz <pcmoritz@gmail.com>
2025-03-24 00:00:43 +0000

    fix test

--- test/test_tt.py
+++ test/test_tt.py
@@ -1,17 +1,15 @@
 import torch
 import unittest
-from parameterized import parameterized
 
-class TestTTDevice(unittest.TestCase):
-    @parameterized.expand([
-        ("float32", torch.float32),
-        ("bfloat16", torch.bfloat16),
-    ])
-    def test_to_tt_and_back(self, name, dtype):
-        a = torch.ones(100000, dtype=dtype)
-        b = a.to("tt")
-        c = b.to("cpu")
-        self.assertTrue((a == c).all())
+class TestTT(unittest.TestCase):
+
+    def test_tt_roundtrip(self):
+        for dtype in [torch.bfloat16, torch.float32]:
+            a = torch.ones(100000, dtype=dtype)
+            b = a.to("tt")
+            c = b.to("cpu")
+            self.assertTrue((a == c).all())
 
 if __name__ == "__main__":
     unittest.main()
+
