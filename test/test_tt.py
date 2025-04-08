92217e7402567ec46138b1eb8db28c60985c11af
Philipp Moritz <pcmoritz@gmail.com>
2025-04-08 21:24:01 +0000

    update

--- test/test_tt.py
+++ test/test_tt.py
@@ -3,14 +3,12 @@ import unittest
 
 class TestTT(unittest.TestCase):
 
-    """
     def test_tt_roundtrip(self):
         for dtype in [torch.bfloat16, torch.float32]:
             a = torch.ones(100000, dtype=dtype)
             b = a.to("tt")
             c = b.to("cpu")
             self.assertTrue((a == c).all())
-    """
 
     def test_tt_add(self):
         a = torch.ones(32 * 32, dtype=torch.bfloat16)
