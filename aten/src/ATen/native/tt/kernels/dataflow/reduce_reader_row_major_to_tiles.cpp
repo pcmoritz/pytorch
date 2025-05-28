eabe0c552f95e721a20f870538c450a99c7c401d
Philipp Moritz <pcmoritz@gmail.com>
2025-05-28 05:52:39 +0000

    factor out utils

--- aten/src/ATen/native/tt/kernels/dataflow/reduce_reader_row_major_to_tiles.cpp
+++ aten/src/ATen/native/tt/kernels/dataflow/reduce_reader_row_major_to_tiles.cpp
@@ -1,39 +1,14 @@
 #include <stdint.h>
 #include "dataflow_api.h"
 
+#include "util.h"
+
 constexpr uint32_t TILE_HEIGHT = 32;
 constexpr uint32_t TILE_WIDTH = 32;
 
 constexpr uint32_t FACE_HEIGHT = 16;
 constexpr uint32_t FACE_WIDTH = 16;
 
-// Tile is assumed to have 16-bit elements
-// Scaler is assumed to be a 16-bit value double packed into a u32
-FORCE_INLINE void generate_mm_scaler(const uint32_t cb_id, const uint32_t scaler) {
-    cb_reserve_back(cb_id, 1);
-
-    constexpr uint32_t num_zeros_reads = 2048 / MEM_ZEROS_SIZE;
-    uint64_t zeros_noc_addr = get_noc_addr(MEM_ZEROS_BASE);
-    uint32_t write_addr = get_write_ptr(cb_id);
-    volatile tt_l1_ptr uint32_t* ptr = reinterpret_cast<volatile tt_l1_ptr uint32_t*>(write_addr);
-
-    // Fill tile with zeros
-    // TODO: src addr does not need to be rewritten. Update/add api for this
-    noc_async_read_one_packet_set_state(zeros_noc_addr, MEM_ZEROS_SIZE);
-    for (uint32_t i = 0; i < num_zeros_reads; ++i) {
-        noc_async_read_one_packet_with_state(zeros_noc_addr, write_addr);
-        write_addr += MEM_ZEROS_SIZE;
-    }
-    noc_async_read_barrier();
-
-    // Fill the first row of the tile with scales
-    for (int i = 0; i < 8; ++i) {
-        ptr[i] = scaler;
-        ptr[i + 128] = scaler;
-    }
-
-    cb_push_back(cb_id, 1);
-}
 
 void kernel_main() {
     uint32_t src_addr = get_arg_val<uint32_t>(0);
